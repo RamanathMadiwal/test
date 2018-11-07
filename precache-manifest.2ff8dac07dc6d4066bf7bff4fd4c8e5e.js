2349effe8a4e620a260a7b2d671210db34a96728
Ramanath Madiwal <ramanathrm.kumta@gmail.com>
2018-11-07 18:22:33 +0530

    Updates

---
+++ precache-manifest.2ff8dac07dc6d4066bf7bff4fd4c8e5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/RamanathMadiwal/test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0b23380b66171b652814",
+    "url": "/RamanathMadiwal/test/static/js/runtime~main.0b23380b.js"
+  },
+  {
+    "revision": "aacadfeee28bf577dc41",
+    "url": "/RamanathMadiwal/test/static/js/main.aacadfee.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/RamanathMadiwal/test/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "aacadfeee28bf577dc41",
+    "url": "/RamanathMadiwal/test/static/css/main.480121ac.chunk.css"
+  },
+  {
+    "revision": "09d1cca360e55b36a3c706e54e5aea96",
+    "url": "/RamanathMadiwal/test/index.html"
+  }
+];
